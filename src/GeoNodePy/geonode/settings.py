f057ae3a94a9ee46bb07a8c8ad1aca9692ddc7ae
Francesco Stompanato <francesco.stompanato@wfp.org>
2012-02-28 05:47:09 -0800

    Added rating features. Remember to adda a symlink to the agon_ratings static files and to import jquery

--- src/GeoNodePy/geonode/settings.py
+++ src/GeoNodePy/geonode/settings.py
@@ -122,6 +122,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
+    "django.core.context_processors.request",
     "geonode.maps.context_processors.resource_urls",
 )
 
@@ -224,11 +225,14 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.sitemaps',
+    
     'staticfiles',
     'django_extensions',
     'registration',
     'profiles',
     'avatar',
+    'agon_ratings',
+    
     'south',
     'geonode.core',
     'geonode.maps',
@@ -268,6 +272,17 @@ DB_DATASTORE_TYPE=''
 SOUTH_MIGRATION_MODULES = {
     'registration': 'geonode.migrations.registration',
     'avatar': 'geonode.migrations.avatar',
+}
+
+
+# Agon Ratings
+AGON_RATINGS_CATEGORY_CHOICES = {
+  "maps.Map": {
+    "map": "How good is this map?"
+  },
+  "maps.Layer": {
+    "layer": "How good is this layer?"
+  },
 }
 
 try:
